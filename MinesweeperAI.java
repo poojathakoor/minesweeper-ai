e63e24cce66233b638aae07a4764adcf7899b3e1
Pooja <pooja.mt18@gmail.com>
2019-01-27 01:33:22 -0800

    Update MinesweeperAI.java

--- MinesweeperAI.java
+++ MinesweeperAI.java
@@ -1,22 +1,4 @@
 
-/*
-
-AUTHOR:      John Lu
-
-DESCRIPTION: This file contains your agent class, which you will
-             implement.
-
-NOTES:       - If you are having trouble understanding how the shell
-               works, look at the other parts of the code, as well as
-               the documentation.
-
-             - You are only allowed to make changes to this portion of
-               the code. Any changes to other portions of the code will
-               be lost when the tournament runs your code.
-*/
-
-
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -30,24 +12,7 @@ import java.util.Stack;
 
 
 public class MinesweeperAI {
-	// ########################## INSTRUCTIONS ##########################
-	// 1) The Minesweeper Shell will pass in the board size, number of mines
-	// and first move coordinates to your agent. Create any instance variables
-	// necessary to store these variables.
-	//
-	// 2) You MUST implement the getAction() method which has a single
-	// parameter,
-	// number. If your most recent move is an Action.UNCOVER action, this value
-	// will
-	// be the number of the tile just uncovered. If your most recent move is
-	// not Action.UNCOVER, then the value will be -1.
-	//
-	// 3) Feel free to implement any helper functions.
-	//
-	// ###################### END OF INSTURCTIONS #######################
-
-	// This line is to remove compiler warnings related to using Java generics
-	// if you decide to do so in your implementation.
+	
 	int rowDimension, colDimension;
 	int totalMines, startX, startY, currentX, currentY, mineCounter;
 	Location[][] board;
@@ -64,8 +29,7 @@ public class MinesweeperAI {
 	boolean verbose = false;
 
 	public MinesweeperAI(int rowDimension, int colDimension, int totalMines, int startX, int startY) {
-		// ################### Implement Constructor (required)
-		// ####################
+		
 		this.rowDimension = colDimension;
 		this.colDimension = rowDimension;
 		this.totalMines = totalMines;
@@ -113,9 +77,6 @@ public class MinesweeperAI {
 		// game difficulty
 		this.difficulty = findDifficulty(this.totalMines);
 	}
-
-	// ################## Implement getAction(), (required)
-	// #####################
 
 	public Action getAction(int number) {
 		
